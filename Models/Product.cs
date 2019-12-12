80befd90565b4875c2dbfde1d415fb8d4085e10f
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2019-12-12 12:32:12 +0530

    Create Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Stock { get; set; }
+        
+    }
+}
