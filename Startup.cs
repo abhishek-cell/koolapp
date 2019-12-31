7f271244345422d9796195afdb69a1d816f3c9c4
abhishek-cell <58728844+abhishek-cell@users.noreply.github.com>
2019-12-31 16:55:12 +0530

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using KoolApplicationMain.Models;
 
 namespace KoolApplicationMain
 {
@@ -33,6 +34,7 @@ namespace KoolApplicationMain
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IProductInformation, Search>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
